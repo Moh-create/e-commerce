92cc26470b4f94618f059357970e12a685d79997
Ansar <moha.ans@hotmail.com>
2020-12-09 17:32:13 +0100

    commit : ajout navbar, contenu

--- View/index.php
+++ View/index.php
@@ -7,11 +7,28 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="../resources/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Capriola&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Baloo+Tammudu+2:wght@500&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Ramabhadra&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Convergence&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Convergence&display=swap" rel="stylesheet">
     <title>Hello, world!</title>
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #3C5951;">
+  <nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #80DDF2;">
   <div class="container">
     <a class="navbar-brand" href="#">PICKUP ME</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -39,25 +56,84 @@
 
       </ul>
       <form class="d-flex ms-5">        
-        <button class="btn btn-outline-success me-3" type="submit">Connexion</button>
-        <button class="btn btn-outline-success" type="submit">Inscription</button>
+        <button class="btn btn-color me-3" type="submit">Connexion</button>
+        <button class="btn btn-color" type="submit">Inscription</button>
       </form>
     </div>
   </div>
 </nav>
 
 <section id="haut-de-page">
-    <h1>recevez vos colis en Afrique en toute simplicité</h1>
-    <h3>acheter vos articles et on s'occupe de les envoyer</h3>
+    <div class="container pt-5">
+        <div class="info ">
+            <div class="text-center">
+                <h1 class="pb-2 text-uppercase">Recevez vos colis en Afrique
+                   <br>  de manière très simple</h1>
+                <h3>Acheter vos articles et on s'occupe de les envoyer</h3>                
+            </div>
+ 
+        </div>
+        <div class="image-haut-de-page">
+            <div class="text-center">
+                <img src="../resources/images/—Pngtree—hand drawn wind cartoon cute_4045140.png" class="img-fluid" alt="Responsive image" > 
+            </div>
+            
+        </div>
 
+    </div>
+</section>
+
+<section class="coupe"></section>
+<br><br><br><br><br><br><br>
+
+<section id="contenu-presentation pt-5">
+    <div class="container">
+
+        <div class="row align-items-start">
+            <div class="info pt-5">
+                <div class="text-center pt-5">
+                    <h2 class="text-uppercase">qui sommes nous ?</h2>
+                </div>
+            </div>  
+        </div>
+
+
+        
+    <div class="row align-items-center pt-5">
+
+        <div class="col-lg-6 col-sm-12">
+        <img src="../resources/images/5469.jpg" alt=""class="img-fluid" alt="...">
+        </div>
+
+        <div class="col-lg-6 col-sm-12">
+        <p class ="entreprise_presentation">PICKME UP est une start up qui propose des services de livraison pour les personnes en
+Afrique souhaitant faire des achats sur des sites internet de vente en ligne internationaux.</p>
+        </div>
+
+ 
+    </div>
+
+
+    </div>
+</section>
+
+<section id="contenu-fonctionnement">
+    <div class="container">
+    <div class="row align-items-start">
+            <div class="info pt-5">
+                <div class="text-center pt-5">
+                    <h2 class="text-uppercase">comment ça marche ?</h2>
+                </div>
+            </div>  
+        </div>
+    </div>
 </section>
 
 
 
 
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-
-
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
  
